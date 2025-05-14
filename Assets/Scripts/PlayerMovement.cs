17a17f545e9b29f2706e047af8b62bc799c219f8
Galagyy <109639693+Galagyy@users.noreply.github.com>
2025-05-13 22:36:44 -0700

    [Add] Player Movement
    Add player movement in the "Game" scene for WASD and arrow keys as well as controller support.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    public void Start() {
+        this.rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update() {
+        rb.linearVelocity = (moveInput).normalized * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        this.moveInput = context.ReadValue<Vector2>();
+    }
+}
