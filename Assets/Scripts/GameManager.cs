642951a57ad2034be05361cb95cf88039ed7cbba
Galagyy <109639693+Galagyy@users.noreply.github.com>
2025-05-17 22:57:23 -0700

    [Add] Music Persistance & Transition
    - Music persists across Credits and Menu
    - Music transitions when game starts.
    - Background animation stub.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public static GameManager instance;
+
+    private AudioSource musicSource;
 
     public const string version = "1.1.0";
     public const bool developmentBuild = true;
@@ -9,15 +12,21 @@ public class GameManager : MonoBehaviour {
 
     public float timeOfDay = 0.0f;
 
-    void Start() {
+    void Awake() {
         if (instance == null) {
             lock (this) {
                 instance = this;
+
+                this.musicSource = GetComponent<AudioSource>();
+
                 DontDestroyOnLoad(gameObject);
             }
         } else {
             Destroy(gameObject);
         }
+    }
+    public static GameManager GetInstance() {
+        return instance;
     }
 
     void Update(){
@@ -26,5 +35,31 @@ public class GameManager : MonoBehaviour {
         if(timeOfDay >= 24f) {
             timeOfDay = 0f;
         }
+    }
+
+    public void PlayMusic() {
+        Debug.Log("Playing music.");
+
+        if (!this.musicSource.isPlaying) {
+            this.musicSource.Play();
+        }
+    }
+
+    public IEnumerator FadeOutMusic(int seconds) {
+        float currentVolume = this.musicSource.volume;
+
+        float time = 0f;
+        while(time < seconds) {
+            time += Time.unscaledDeltaTime;
+            this.musicSource.volume = Mathf.Lerp(currentVolume, 0f, time / seconds);
+            yield return null;
+        }
+
+        this.musicSource.volume = 0f;
+        this.musicSource.Stop();
+    }
+    
+    public void StopMusic() {
+        this.musicSource.Stop();
     }
 }
