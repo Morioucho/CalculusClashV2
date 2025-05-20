2aacf1c169bf01e59dad3f877ff072549abc10c0
Saumil Sharma <109639693+Galagyy@users.noreply.github.com>
2025-05-20 08:50:59 -0700

    [Fix] Boss Loader & Data
    - Fix lists import to use System.Collections.Generic.

--- Assets/Scripts/Combat/BossData.cs
+++ Assets/Scripts/Combat/BossData.cs
@@ -1,41 +1,30 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BossData
 {
     private string name;
     private int hp;
-    private list<string> dialogue;
-    private object sprite;
+    private List<string> dialogue;
 
-    public BossData(string name, int hp, List<string> dialogue, object sprite)
-    {
+    public BossData(string name, int hp, List<string> dialogue) {
         this.name = name;
         this.hp = hp;
         this.dialogue = dialogue;
-        this.sprite = sprite;
     }
     
-    public string Name
-    {
+    public string Name {
         get { return name; }
         set { name = value; }
     }
     
-    public int HP
-    {
+    public int HP {
         get { return hp; }
         set { hp = value; }
     }
 
-    public List<string> Dialogue
-    {
+    public List<string> Dialogue {
         get { return dialogue; }
         set { dialogue = value; }
-    }
-
-    public object Sprite
-    {
-        get { return sprite; }
-        set { sprite = value; }
     }
 }
