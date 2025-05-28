a890dfc6da1fbd16047339b1691e27211d9cfdec
Galagyy <109639693+Galagyy@users.noreply.github.com>
2025-05-27 23:55:24 -0700

    [Add] Transition & Camear
    - Add transition system for room changes.
    - Add camera system for following player in larger rooms.

---
+++ Assets/Scripts/Player/Transition.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class TransitionManager : MonoBehaviour {
+    [SerializeField] 
+    public GameObject player;
+    public Image fadeImage;
+
+    public string nextRoom;
+
+    public float xMax;
+    public float xMin;
+
+    public float yMax;
+    public float yMin;
+
+    private const float fadeDuration = 0.6f;
+    private bool isTransitioning = false;
+
+    public void Update() {
+        if (player == null) return;
+
+        Vector2 playerPosition = player.transform.position;
+
+        if (playerPosition.x < xMax && playerPosition.x > xMin &&
+            playerPosition.y < yMax && playerPosition.y > yMin)
+            StartCoroutine(TransitionToNextRoom());
+    }
+
+    private IEnumerator TransitionToNextRoom() {
+        isTransitioning = true;
+
+        fadeImage.gameObject.SetActive(true);
+
+        Color color = fadeImage.color;
+        color.a = 0;
+
+        fadeImage.color = color;
+
+        float elapsed = 0f;
+
+        while (elapsed < fadeDuration) {
+            elapsed += Time.deltaTime;
+
+            float alpha = Mathf.Clamp01(elapsed / fadeDuration);
+
+            color.a = alpha;
+            fadeImage.color = color;
+
+            yield return null;
+        }
+
+        SceneManager.LoadScene(nextRoom);
+    }
+
+    private void debugUtil(Vector2 playerPosition) {
+        // Requires Update method..
+        Debug.Log("Player x position: " + playerPosition.x);
+        Debug.Log("Player y position: " + playerPosition.y);
+
+        Debug.Log("X range: " + xMin + " -> " + xMax);
+        Debug.Log("X range: " + yMin + " -> " + yMax);
+    }
+}
