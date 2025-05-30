fdb18e0c6b92a64435b94fd045b54a1ade07ef0d
Galagyy <109639693+Galagyy@users.noreply.github.com>
2025-05-29 23:31:13 -0700

    [Hotfix] Enemy Fix 2
    - Add Enemy Data Fix

--- Assets/Scripts/Combat/Util/Enemies/EnemyData.cs
+++ Assets/Scripts/Combat/Util/Enemies/EnemyData.cs
@@ -1,21 +1,21 @@
-using UnityEngine;
-
-[System.Serializable]
-public class EnemyData {
-    public string id;
-    public string name;
-    public int unit;
-    public int health;
-    public DialogueLine[] dialogue;
-    public string[] banter;
-    public string sprite;
-    public string music;
-    public float musicVolume;
-
+    using UnityEngine;
 
     [System.Serializable]
-    public class DialogueLine {
-        public string text;
-        public float duration;
+    public class EnemyData {
+        public string id;
+        public string name;
+        public int[] unit;
+        public int health;
+        public int qt;
+        public DialogueLine[] dialogue;
+        public string[] banter;
+        public string sprite;
+        public string music;
+        public float musicVolume;
+
+        [System.Serializable]
+        public class DialogueLine {
+            public string text;
+            public float duration;
+        }
     }
-}
